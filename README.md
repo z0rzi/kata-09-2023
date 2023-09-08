d69f7367600464e5a5a8a2d21634b06cfc1f5b7f
Vadim Shestopalov <vdmshestopalov@gmail.com>
2023-09-08 10:04:20 +0200

    [fix] CORS fix

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ```
+python -m venv venv
+source venv/bin/activatep
+pip install -r requirements.txt
 uvicorn main:app
 ```
--- src/main.py
+++ src/main.py
@@ -2,9 +2,26 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import date
 from typing import List
-from src.validate import validate_data
+from fastapi.middleware.cors import CORSMiddleware
+
+from validate import validate_data
+
 
 app = FastAPI(title="Unusual Spending API")
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Spending(BaseModel):
