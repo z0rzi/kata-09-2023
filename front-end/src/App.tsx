0eb04bd1acc7a9daf37f61fcbf0d0700a52b6f27
Baptiste Zorzi <baptiste@mbz-dev.com>
2023-09-15 18:21:56 +0200

    [ext] style improvements + clearing cached files

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -27,17 +27,23 @@ function createRandomExpenses() {
         const category =
             categories[Math.floor(Math.random() * categories.length)];
 
+        let price = Math.floor(Math.random() * 1000) / 10;
+
+        if (expenses.length === 666) {
+            price = 1000000;
+        }
+
         expenses.push({
             transaction_id: id++,
             category: category,
             date: new Date(date),
-            price: Math.floor(Math.random() * 1000) / 10,
+            price,
             description: `Spending in category ${category}`
         });
 
         // Increasing the date by anywhere between 0 and 24h
         date.setUTCMinutes(
-            date.getUTCMinutes() + Math.floor(Math.random() * 60 * 24)
+            date.getUTCMinutes() + Math.floor(Math.random() * 60 * 24 * 30)
         );
     }
 
@@ -63,31 +69,127 @@ const CheckButton = styled.button`
     border-radius: 5px;
     border: 0;
     outline: 0;
-    background: #A1CCD1;
+    background: #a1ccd1;
     padding: 3px;
     text-align: center;
     margin-top: 30px;
     cursor: pointer;
 `;
 
+const data = [
+  {
+    "transaction_id": 0,
+    "category": "Technology",
+    "date": "2022-01-01",
+    "price": 66.3,
+    "description": "Spending in category Technology"
+  },
+  {
+    "transaction_id": 1,
+    "category": "Entertainment",
+    "date": "2022-01-05",
+    "price": 11.7,
+    "description": "Spending in category Entertainment"
+  },
+  {
+    "transaction_id": 2,
+    "category": "Insurance",
+    "date": "2022-01-18",
+    "price": 24.6,
+    "description": "Spending in category Insurance"
+  },
+  {
+    "transaction_id": 3,
+    "category": "Food",
+    "date": "2022-01-27",
+    "price": 30.1,
+    "description": "Spending in category Food"
+  },
+  {
+    "transaction_id": 4,
+    "category": "Travel",
+    "date": "2022-02-04",
+    "price": 31.8,
+    "description": "Spending in category Travel"
+  },
+  {
+    "transaction_id": 5,
+    "category": "Travel",
+    "date": "2022-02-17",
+    "price": 12.2,
+    "description": "Spending in category Travel"
+  },
+  {
+    "transaction_id": 6,
+    "category": "Clothing",
+    "date": "2022-03-06",
+    "price": 93.5,
+    "description": "Spending in category Clothing"
+  },
+  {
+    "transaction_id": 7,
+    "category": "Travel",
+    "date": "2022-03-15",
+    "price": 87.6,
+    "description": "Spending in category Travel"
+  },
+  {
+    "transaction_id": 8,
+    "category": "Insurance",
+    "date": "2022-03-26",
+    "price": 46.8,
+    "description": "Spending in category Insurance"
+  },
+  {
+    "transaction_id": 9,
+    "category": "Clothing",
+    "date": "2022-04-15",
+    "price": 67.6,
+    "description": "Spending in category Clothing"
+  }
+]
+
 export default function App() {
-    const [expenses, setExpenses] = useState(createRandomExpenses);
+    const [expenses] = useState(createRandomExpenses());
     const [apiMessage, setApiMessage] = useState('');
+    const [unusualIds, setUnusualIds] = useState(new Set<number>());
 
     function onCheck() {
-        axios.post('http://localhost:8000/validate', expenses).then((message) => {
-            setApiMessage(JSON.stringify(message));
-        });
+        axios
+            .post(
+                'http://localhost:8000/validate',
+                expenses.map((exp) => ({
+                    ...exp,
+                    date: exp.date.toISOString().slice(0, 10)
+                }))
+            )
+            .then((message) => {
+                const messageText = message.data['email_text'] as string;
+                setApiMessage(messageText);
+
+                const ids = new Set<number>();
+                messageText.match(/(?<=transaction_id: )\d+/g).forEach((match) => {
+                    ids.add(+match);
+                });
+                setUnusualIds(ids);
+            })
+            .catch(() => {});
     }
 
     return (
         <Container>
-            <h1 style={{textAlign: 'center', paddingBottom: 40}}>Expenses app</h1>
+            <h1 style={{ textAlign: 'center', paddingBottom: 40 }}>
+                💸 Expenses App 💸
+            </h1>
             <ExpensesContainer>
-                { expenses.map(exp => <ExpenseComponent expense={exp}></ExpenseComponent>) }
+                {expenses.map((exp) => (
+                    <ExpenseComponent unusual={unusualIds.has(exp.transaction_id)} expense={exp}></ExpenseComponent>
+                ))}
             </ExpensesContainer>
-            <CheckButton onClick={onCheck}>Check for unusual spendings</CheckButton>
-            <pre>{ apiMessage }</pre>
+            <CheckButton onClick={onCheck}>
+                Check for unusual spendings
+            </CheckButton>
+            <pre>{apiMessage}</pre>
         </Container>
     );
 }
--- front-end/src/components/expense/Expense.tsx
+++ front-end/src/components/expense/Expense.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 export type Expense = {
@@ -10,26 +10,72 @@ export type Expense = {
 };
 
 type ExpenseProps = {
-    expense: Expense
+    unusual: boolean,
+    expense: Expense;
 };
 
-const Container = styled.div`
+const Container = styled.div<{ $unusual: boolean }>`
+    background-color: ${props => props.$unusual?'#f445':'transparent'};
     display: flex;
     width: 100%;
     justify-content: space-between;
     color: #222;
 `;
 
+const Category = styled.span<{ $color: string }>`
+    padding: 2px 15px;
+    background-color: ${(props) => props.$color};
+    font-weight: bold;
+    color: white;
+    border-radius: 1000px;
+`;
+
+const categoryColors = new Map<string, string>();
+
 function formatDate(date: Date) {
     return date.toISOString().slice(0, 19).replace('T', ' ');
 }
 
-export default function ExpenseComponent(props: PropsWithChildren<ExpenseProps>) {
-    return <Container>
-        <div style={{ opacity: .5, marginRight: 50, flexGrow: 0 }}>#{props.expense.transaction_id}</div>
-        <div style={{ flexGrow: 0 }}>{formatDate(props.expense.date)}</div>
-        <div style={{ flexGrow: 1 }}>{props.expense.category}</div>
-        <div style={{ flexGrow: 1 }}>{props.expense.description}</div>
-        <div style={{ flexGrow: 0, fontWeight: 'bold' }}>{props.expense.price}€</div>
-    </Container>
+function randomDarkHexColor(): string {
+    // Define the range for each color component (0-127 for dark-ish colors)
+    const min = 0;
+    const max = 127;
+
+    // Generate random values for each color component
+    const r = Math.floor(Math.random() * (max - min + 1)) + min;
+    const g = Math.floor(Math.random() * (max - min + 1)) + min;
+    const b = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Convert the RGB values to a hexadecimal color representation
+    const hexColor = `#${r.toString(16).padStart(2, '0')}${g
+        .toString(16)
+        .padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+
+    return hexColor;
+}
+
+export default function ExpenseComponent(
+    props: PropsWithChildren<ExpenseProps>
+) {
+    const category = props.expense.category;
+    if (!(category in categoryColors)) {
+        categoryColors.set(category, randomDarkHexColor());
+    }
+    const color = categoryColors.get(category);
+
+    return (
+        <Container $unusual={props.unusual}>
+            <div style={{ opacity: 0.5, marginRight: 50, flexGrow: 0 }}>
+                #{props.expense.transaction_id}
+            </div>
+            <div style={{ flexGrow: 0 }}>{formatDate(props.expense.date)}</div>
+            <div style={{ textAlign: 'center', flexGrow: 1 }}>
+                <Category $color={color}> {props.expense.category} </Category>
+            </div>
+            <div style={{ flexGrow: 1 }}>{props.expense.description}</div>
+            <div style={{ flexGrow: 0, fontWeight: 'bold' }}>
+                {props.expense.price}€
+            </div>
+        </Container>
+    );
 }
