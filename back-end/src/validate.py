bac42897ce50e86e4976b5243ec5916fde366707
Vadim Shestopalov <vdmshestopalov@gmail.com>
2023-09-15 17:42:37 +0200

    [ext] added transacation_id

--- back-end/src/validate.py
+++ back-end/src/validate.py
@@ -7,11 +7,9 @@ def validate_data(data: list):
     df["month"] = df["date"].dt.month
     df['year'] = df['date'].dt.year
 
-    grouped_df = df.groupby(["month", "category"])['price'].sum().reset_index()
-    print(f"validate_data 0: \n{grouped_df}")
+    grouped_df = df.groupby(["month", "category", "transaction_id"])['price'].sum().reset_index()
     grouped_df['percentage_change'] = grouped_df.groupby("category")['price'].pct_change().fillna(0)
     grouped_df['month_diff'] = grouped_df.groupby("category")["month"].diff().fillna(0)
-    print(f"validate_data 1: \n{grouped_df}")
     # grouped_df['percentage_change_month'] = grouped_df.groupby("month")['price'].pct_change().fillna(0)
     # print(f"validate_data 1: \n{grouped_df}")
     # grouped_df['change_month'] = grouped_df.groupby('category')['price'].pct_change().fillna(0)
@@ -23,7 +21,8 @@ def validate_data(data: list):
                 "month": d["month"],
                 "price": d["price"],
                 "category": d["category"],
-                "percentage_change": d["percentage_change"]
+                "percentage_change": d["percentage_change"],
+                "transaction_id": d["transaction_id"]
             }
         )
     return res
@@ -32,6 +31,6 @@ def validate_data(data: list):
 def create_email_text(res):
     text = "future email text: \n"
     for i in res:
-        text+=f"price: {i['price']}, pct_change: {i['percentage_change']} category: {i['category']}, month: {i['month']} \n"
+        text+=f"transaction_id: {i['transaction_id']}, price: {i['price']}, pct_change: {i['percentage_change']} category: {i['category']}, month: {i['month']} \n"
     return text
 
